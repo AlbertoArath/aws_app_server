745c48b0bf2b63793700ec17a05bcb2ac1f14f0c
aafs <afiguerog@gmail.com>
2025-03-15 19:00:13 -0600

    Fix server

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const express = require("express");
-const path = require("path");
+const http = require("http");
+const fs = require("fs");
 
-const app = express();
-const PORT = 3000;
-
-// Serve static files (optional, for CSS, images)
-app.use(express.static("public"));
-
-// Serve index.html when accessing the root URL
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+http.createServer((req, res) => {
+    fs.readFile("index.html", (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error loading page");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        }
+    });
+}).listen(80, () => console.log("Server running on port 80"));
