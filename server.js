1d3d3311bdfc450b93735a808e094e27d7a5b188
aafs <afiguerog@gmail.com>
2025-04-04 17:06:36 -0600

    Test

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const http = require("http");
+const https = require("https");
 const fs = require("fs");
 
-http.createServer((req, res) => {
-    fs.readFile("index.html", (err, data) => {
+// Read the certificate files
+const options = {
+    key: fs.readFileSync("private.key"),
+    cert: fs.readFileSync("certificate.crt"),
+};
+
+// Create an HTTPS server
+https.createServer(options, (req, res) => {
+    fs.readFile("AboutMe.html", (err, data) => {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/plain" });
             res.end("Error loading page");
@@ -11,4 +18,4 @@ http.createServer((req, res) => {
             res.end(data);
         }
     });
-}).listen(80, () => console.log("Server running on port 80"));
+}).listen(443, () => console.log("Secure server running on port 443"));
